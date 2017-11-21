bbdcbb82cff13f822a79d92cc7c9274c49bc5682
coffeeandscripts <erin.saricilar@gmail.com>
2017-11-21 23:26:40 +1100

    Basic class setup

--- prntspot.cpp
+++ prntspot.cpp
@@ -1,3 +1,22 @@
-// prntspot.cpp
-// desc: prntspot engine with functions and variables
+// prntspot.h
+// desc: header for functions and classes
 // vers: 0.01
+
+#include "prntspot.h"
+
+
+prntspot::prntspot() {
+
+}
+
+int prntspot::max_height() {
+
+}
+
+int prntspot::max_width() {
+
+}
+
+void prntspot::set_window_size() {
+
+}
--- prntspot.h
+++ prntspot.h
@@ -1,3 +1,14 @@
-// prntspot.h
-// desc: header for functions and classes
+// prntspot.cpp
+// desc: prntspot engine with functions and variables
 // vers: 0.01
+
+class prntspot {
+	public:
+	prntspot();
+	int max_height();
+	int max_width();
+	private:
+	int window_width;
+	int window_height;
+	void set_window_size();
+};
