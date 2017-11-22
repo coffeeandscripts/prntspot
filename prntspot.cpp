91b3f105757917995131aefc5761c4652352fb1f
coffeeandscripts <erin.saricilar@gmail.com>
2017-11-22 15:28:28 +1100

    Setting window size

--- prntspot.cpp
+++ prntspot.cpp
@@ -2,21 +2,26 @@
 // desc: header for functions and classes
 // vers: 0.01
 
+#include <sys/ioctl.h>
+
 #include "prntspot.h"
 
 
 prntspot::prntspot() {
-
+	set_window_size();
 }
 
 int prntspot::max_height() {
-
+	return window_height;
 }
 
 int prntspot::max_width() {
-
+	return window_width;
 }
 
 void prntspot::set_window_size() {
-
+	struct winsize w;
+	ioctl(0, TIOCGWINSZ, &w);
+	window_width = w.ws_row;
+	window_height = w.ws_col;
 }
--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,13 @@
 // tests.cpp
 // desc: main function to run tests for prntspot
 // vers: 0.01
+
+#include <iostream>
+
+#include "prntspot.h"
+
+int main() {
+	prntspot printer;
+	std::cout << "Height: " << printer.max_height() << '\n';
+	std::cout << "Width:  " << printer.max_width() << '\n';
+}
